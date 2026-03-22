ee79113e21e4be5dacef18e3a3595ae8ba3c721b
Joao Pedro Rosa <joao@bucc.nl>
2026-03-22 15:41:58 +0100

    Delete serve-public.ps1

--- serve-public.ps1
+++
@@ -1,4 +0,0 @@
-# Double-click or: powershell -ExecutionPolicy Bypass -File .\serve-public.ps1
-Set-Location $PSScriptRoot
-python -m pip install -r requirements.txt -q 2>$null
-python serve_public.py
