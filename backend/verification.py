1b35ec86738618efec8fa23e810c0877e5abea2d
KaChun Cho <kachun.cho@arup.com>
2026-03-23 07:19:50 -0400

    refactored the check and calc functions, output json added "Dimension Fixable" and "Material Calcuable"

--- backend/verification.py
+++ backend/verification.py
@@ -1,12 +1,45 @@
 import json
 import os
 
+def _check_if_IfcBeam_IfcColumn_dimension_fixable(v):
+    deflect = None
+    count_para = [0,0,0]
+    values = ["Length", "CrossSectionArea", "Volume"]
+    for n, para in enumerate(values):
+        if para in v["dimensions"]:
+            count_para[n] = 1
+    if count_para.count(1) <= 1:
+        fixable = False
+        return(fixable, None)
+    elif count_para.count(1) == 2:
+        fixable = True
+    else:
+        fixable = None
+    if fixable == True:
+        if count_para[0] == 0:
+            v["dimensions"]["Length"] = v["dimensions"]["Volume"] / v["dimensions"]["CrossSectionArea"]
+            deflect = "Length"
+        elif count_para[1] == 0:
+            v["dimensions"]["CrossSectionArea"] = v["dimensions"]["Volume"] / v["dimensions"]["Length"]
+            deflect = "CrossSectionArea"
+        elif count_para[2] == 0:
+            v["dimensions"]["Volume"] = v["dimensions"]["Length"] * v["dimensions"]["CrossSectionArea"]
+            deflect = "Volume"
+    return(fixable, deflect)
 
-def _calc_length(volume, cross_section_area):
-    if cross_section_area != 0:
-        return volume / cross_section_area
-    return None
-
+def _calc_material_attributes(v):
+    #print(v["materials"][0]['properties']['Materials and Finishes])
+    mat_v = v["materials"][0]['properties']
+    material_type = mat_v["Materials and Finishes"]["Material Type"]
+    try:
+        if material_type == "Steel" or material_type == "Wood":
+            mat_v["Pset_MaterialCommon"]["Weight"] = ( mat_v["Pset_MaterialCommon"]["MassDensity"] * v["dimensions"]["Volume"] * 9.81)
+        elif material_type == "Concrete":
+            mat_v["Pset_MaterialCommon"]["Weight"] = v["dimensions"]["Volume"]
+        return(True)
+    except KeyError as ex:
+        print(ex)
+        return (False)
 
 def run_verification(input_json_path: str, output_dir: str) -> dict:
     """
@@ -19,28 +52,23 @@ def run_verification(input_json_path: str, output_dir: str) -> dict:
     """
     with open(input_json_path, "r", encoding="utf-8") as f:
         ifc_json_data = json.load(f)
-
     defect_dict_class = {}
-
     for value in ifc_json_data.values():
         defect_list = []
-        if value["class"] in ("IfcBeam", "IfcColumn"):
-            if "Length" not in value["dimensions"]:
-                defect_list.append("Length")
-                value["dimensions"]["Length"] = _calc_length(
-                    value["dimensions"]["Volume"],
-                    value["dimensions"]["CrossSectionArea"],
-                )
-        if defect_list:
-            cls = value["class"]
-            gid = value["globalId"]
-            defect_dict_class.setdefault(cls, []).append({gid: defect_list})
-
+        if value["class"] == "IfcBeam" or value["class"] == "IfcColumn":
+                dim_fixable, dim_defect = _check_if_IfcBeam_IfcColumn_dimension_fixable(value)
+                defect_list.append(dim_defect)
+                mat_calcuable = _calc_material_attributes(value)
+        
+        cls = value["class"]
+        gid = value["globalId"]
+        defect_dict_class.setdefault(cls, []).append({gid: {"Dimension Fixable": dim_fixable, "Material Calcuable": mat_calcuable, "Attributes":defect_list}})
+            
     basename = os.path.basename(input_json_path)
     name_stem = os.path.splitext(basename)[0]
     corrected_filename = f"{name_stem}_corrected.json"
     corrected_path = os.path.join(output_dir, corrected_filename)
-    log_path = os.path.join(output_dir, "verification_log.json")
+    log_path = os.path.join(output_dir, f"{name_stem}_verification_log.json")
 
     with open(corrected_path, "w", encoding="utf-8") as f:
         json.dump(ifc_json_data, f, indent=4)
